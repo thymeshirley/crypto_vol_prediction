b0c20220f7f3cc34f86669b202cbd5dc88f15147
Tim Lu <wind.zju@hotmail.com>
2022-10-30 16:27:20 +0000

    dataloader

--- crypto_vol_prediction/base/base_data_loader.py
+++ crypto_vol_prediction/base/base_data_loader.py
@@ -1,10 +1,22 @@
 from torch.utils.data import DataLoader
-
+import pandas as pd
 
 class DataLoaderBase(DataLoader):
     """
     Base class for all data loaders
     """
+
+    def refresh_from_remote(self):
+        """
+        Refresh local data cache from remote database
+        """
+        raise NotImplementedError
+
+    def get_dataframe(self) -> pd.DataFrame:
+        """
+        Return dataframe from raw data
+        """
+        raise NotImplementedError
 
     def split_validation(self) -> DataLoader:
         """
--- crypto_vol_prediction/data_loader/data_loaders.py
+++ crypto_vol_prediction/data_loader/data_loaders.py
@@ -1,10 +1,49 @@
 import numpy as np
+import yaml
+import os
+import psycopg2
+import pandas as pd
 from torch.utils.data import DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
 from torchvision import datasets
 
 from crypto_vol_prediction.base import DataLoaderBase
 
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+class UniswapV2SwapTranLoader(DataLoaderBase):
+    """
+    Loading Uniswap V2 swap transactions
+    """
+    def __init__(self, pair, data_dir):
+        self.pair = pair
+        self.data_dir = data_dir
+        self.data_cache_name = f"UniswapV2SwapTranLoader_{self.pair}"
+        self.data_cache_path = os.path.join(self.data_dir, self.data_cache_name)
+    
+    def refresh_from_remote(self):
+        with open(os.path.join(__location__, "dbconfig.yaml"), "r") as conf_file:
+            try:
+                db_config = yaml.safe_load(conf_file)
+                postgresql_config = db_config['connections']['postgresql']
+                hostname = postgresql_config['hostname']
+                port = postgresql_config['port']
+                dbname = postgresql_config['dbname']
+                user = postgresql_config['user']
+                password = postgresql_config['password']
+                connection_str = f"host={hostname} port={port} dbname={dbname} user={user} password={password} sslmode=disable"
+                with psycopg2.connect(connection_str) as conn:
+                    cur = conn.cursor()
+                    query = """SELECT * FROM public.uniswap_v2_swap_transactions LIMIT 100"""
+                    output_query = f"COPY ({query}) TO STDOUT WITH CSV HEADER"
+                    with open(self.data_cache_path, 'w') as cache_file:
+                        cur.copy_expert(output_query, cache_file)
+            except yaml.YAMLError as e:
+                print(e)
+
+    def get_dataframe(self):
+        return pd.read_csv(self.data_cache_path)
 
 class MnistDataLoader(DataLoaderBase):
     """
