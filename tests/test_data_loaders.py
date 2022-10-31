1cb5983aecefcd0db8d26f4d58637e6e737ecc01
Tim Lu <wind.zju@hotmail.com>
2022-10-31 12:12:54 +0000

    update

--- tests/test_data_loaders.py
+++ tests/test_data_loaders.py
@@ -6,7 +6,7 @@ __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 def test_uniswap_dataloader():
-    loader = UniswapV2SwapTranLoader("dummy", os.path.join(__location__, "data_cache"))
-    # loader.refresh_from_remote()
+    loader = UniswapV2SwapTranLoader("dummy", os.path.join(__location__, "..", "data_cache"))
+    loader.refresh_from_remote()
     df = loader.get_dataframe()
     print(df.head())
